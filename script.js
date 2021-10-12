463c609334563c3a3a05d5f5c3e7fe8d28ab595c
Kunal Giradkar <33kunalg@gmail.com>
2021-10-12 21:21:50 +0530

    yte PushJS

---
+++ script.js
@@ -0,0 +1,11 @@
+const dolphinsScore = 96 + 108 + 89;
+const koalasScore = 88 + 91 + 110;
+const avgdolphins = dolphinsScore / 2;
+const avgkoalas = koalasScore / 2;
+console.log(avgdolphins);
+console.log(avgkoalas);
+if (avgdolphins > avgkoalas) {
+  console.log(`dolphins are winner`);
+} else {
+  console.log(`avgkoalas are winner`);
+}
