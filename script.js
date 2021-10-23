9884c263d5b897792cd2051b4a773c144c143a55
Kunal Giradkar <33kunalg@gmail.com>
2021-10-23 08:47:50 +0530

    octlast 23rdoct

--- script.js
+++ script.js
@@ -81,10 +81,10 @@ const lastName = 'Schmedtmann';
 console.log(firstName + ' ' + lastName);
 
 // Assignment operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
+let x = 10 + 5; 
+x += 10; 
+x *= 4; 
+x++;
 x--;
 x--;
 console.log(x);
