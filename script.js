f88d39e2200cd836de3980b97230bc83ca9f40de
Kunal Giradkar <33kunalg@gmail.com>
2021-11-17 08:47:50 +0530

    private 80hajar

--- script.js
+++ script.js
@@ -300,11 +300,7 @@ console.log(hasDriversLicense && hasGoodVision);
 console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasDriversLicense);
 
-// if (hasDriversLicense && hasGoodVision) {
-//   console.log('Sarah is able to drive!');
-// } else {
-//   console.log('Someone else should drive...');
-// }
+
 
 const isTired = false; // C
 console.log(hasDriversLicense && hasGoodVision && isTired);
