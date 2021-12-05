50b319066cbc2d2aeae120fab869949970f2e2b0
Kunal Giradkar <33kunalg@gmail.com>
2021-12-05 08:47:50 +0530

    5 5dec

--- script.js
+++ script.js
@@ -279,7 +279,7 @@ const favourite = Number(prompt("What's your favourite number?"));
 console.log(favourite);
 console.log(typeof favourite);
 
-if (favourite === 23) { // 22 === 23 ->
+if (favourite === 23) { 
   console.log('Cool! 23 is an amzaing number!')
 } else if (favourite === 7) {
   console.log('7 is also a cool number')
